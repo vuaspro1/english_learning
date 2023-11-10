224c8bc48054d0ece80f101382ecd780f57ba9f4
vuaspro1 <vuaspro1@gmail.com>
2023-11-10 12:19:59 +0700

    Update HomeScreen 11/10/2023

--- lib/src/constants/colors.dart
+++ lib/src/constants/colors.dart
@@ -1,8 +1,6 @@
 import 'dart:ui';
 
-const tPrimaryColor = Color(0xFFFFE400);
 const tSecondaryColor = Color(0xFF272727);
-const tAccentColor = Color(0xFF001BFF);
 
 const tCardBgColor = Color(0xFFF7F6F1);
 const tWhiteColor = Color(0xffffffff);
@@ -18,6 +16,5 @@ const tErrorColor = Color(0xFFFD0F01);
 
 //home
 const tBoxDecoration =Color(0xFF8AEAE9);
-const tStart =Color(0xDD000000);
 const tviewtabbar = Color(0xFFF5F3FF);
 const tSuccessfullyColor = Color(0xFF189409);
